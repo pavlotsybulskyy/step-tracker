85ad3e9f9cacf246eae1e3aaa5f8fcfd87b716a8
Pavlo Tsybulskyi <pavlotcubylski@gmai.com>
2025-08-24 22:41:00 +0300

    Add weight diff calculations

--- Step Tracker/Charts/ChartMath.swift
+++ Step Tracker/Charts/ChartMath.swift
@@ -24,4 +24,28 @@ struct ChartMath {
         
         return weekdayChartData
     }
+    
+    static func averageDailyWeightDifference(for weights: [HealthMetric]) -> [WeekdayChartData] {
+        var diffValues: [(date: Date, value: Double)] = []
+        
+        for i in 1..<weights.count {
+            let date = weights[i].date
+            let diff = weights[i].value - weights[i - 1].value
+            diffValues.append((date: date, value: diff))
+        }
+        
+        let sortedByWeekday = diffValues.sorted { $0.date.weekday < $1.date.weekday }
+        let weekdayArray = sortedByWeekday.chunked { $0.date.weekday == $1.date.weekday }
+        
+        var weekdayChartData: [WeekdayChartData] = []
+        for array in weekdayArray {
+            guard let first = array.first else { continue }
+            let total = array.reduce(0) { $0 + $1.value }
+            let avgWeightDiff = total / Double(array.count)
+            
+            weekdayChartData.append(WeekdayChartData(date: first.date, value: avgWeightDiff))
+        }
+        
+        return weekdayChartData
+    }
 }
